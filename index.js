fe5604a786093a654a360d3f966a75766d2b75fb
nattyKhoe <natasha.kusuma@outlook.com>
2022-06-22 21:48:47 +1000

    Done but not passed in learn test code

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Add your code here
+window.onload = function (){
+    //Dog Server
+    
+    // const formData = {
+    //     dogName: "Byron",
+    //     dogBreed: "Poodle"
+    // };
+    
+    // const configObj = {
+    //     method: "POST",
+    //     headers:{
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json"
+    //     },
+    //     body: JSON.stringify(formData)
+    // }
+    
+    // const error = {message: "Something went wrong!"};
+    
+    // fetch("http://localhost:3000/dogs", configObj)
+    // .then (function(response){
+    //     return response.json();
+    // })
+    // .then (function(json){
+    //     console.log(json)
+    // })
+    // .catch(function(error) {
+    //     alert("Bad things! Ragnarők!");
+    //     console.log(error.message);
+    //   });
+    
+    //Test 1 - Send Data users
+    //Test 2- Handle the Response
+    
+    const submitData = ( name, email ) => {
+        let userData = {
+            'name' : name,
+            'email' : email
+        };
+        window.fetch("http://localhost:3000/users",{
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+                "Accept": "application/json"
+            }
+        ,
+        body: JSON.stringify(userData)
+        })
+        .then(response => response.json)
+        .then (json => console.log(json))
+        .catch(function(error){
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+        })
+    };
+    submitData("Natasha","nat@hotmail.com");
+};
+    
+    
